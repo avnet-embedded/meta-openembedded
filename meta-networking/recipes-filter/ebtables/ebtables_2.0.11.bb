avnet-embedded/meta-openembedded

commit 584fec0f74ad7c3ebe3bc5794be93382d4d386eb

ebtables: Use update alternatives for "ebtables"
The ebtables utility can be provided by both ebtables and iptables
packages. Set higher priority for the version provided by iptables
to prefer it.

Signed-off-by: Zhang Xiao <xiao.zhang@windriver.com>
Signed-off-by: Khem Raj <khem.raj@oss.qualcomm.com>

meta-networking/recipes-filter/ebtables/ebtables_2.0.11.bb
@@ -21,7 +21,7 @@ SRC_URI:append:libc-musl = " file://0010-Adjust-header-include-sequence.patch"
 SRC_URI[md5sum] = "071c8b0a59241667a0044fb040d4fc72"
 SRC_URI[sha256sum] = "b71f654784a726329f88b412ef7b96b4e5d786ed2bd28193ed7b4c0d677dfd2a"
 
-inherit systemd autotools
+inherit systemd autotools update-alternatives
 
 do_install:append () {
 	# Replace upstream ebtables-save perl script with Fedora bash based rewrite
@@ -49,3 +49,7 @@ FILES:${PN}-dbg += "${base_libdir}/ebtables/.debug"
 FILES:${PN} += "${base_libdir}/ebtables/*.so"
 
 SYSTEMD_SERVICE:${PN} = "ebtables.service"
+
+ALTERNATIVE:${PN} = "ebtables"
+ALTERNATIVE_LINK_NAME[ebtables] = "${sbindir}/ebtables"
+ALTERNATIVE_PRIORITY[ebtables] = "90"

commit 88c22e566d0e0e1997edb6ac5f51a7818366d6cd

ebtables: Fix update-alternatives by setting ALTERNATIVE_TARGET
The commit adding update-alternatives support omitted
ALTERNATIVE_TARGET[ebtables], causing the bbclass to fall back to
constructing the target as ${sbindir}/ebtables.ebtables which does
not exist. The binary is installed as ebtables-legacy, so set
ALTERNATIVE_TARGET accordingly.

fixes QA warnings:

  ebtables: alternative target does not exist, skipping
  ebtables: NOT adding alternative provide /usr/sbin/ebtables
  ebtables: alt_link == alt_target: /usr/sbin/ebtables == /usr/sbin/ebtables

Fixes: 584fec0f74ad ("ebtables: Use update alternatives for "ebtables"")

Signed-off-by: Khem Raj <khem.raj@oss.qualcomm.com>

meta-networking/recipes-filter/ebtables/ebtables_2.0.11.bb
@@ -52,4 +52,5 @@ SYSTEMD_SERVICE:${PN} = "ebtables.service"
 
 ALTERNATIVE:${PN} = "ebtables"
 ALTERNATIVE_LINK_NAME[ebtables] = "${sbindir}/ebtables"
+ALTERNATIVE_TARGET[ebtables] = "${sbindir}/ebtables-legacy"
 ALTERNATIVE_PRIORITY[ebtables] = "90"